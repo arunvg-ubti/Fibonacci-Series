4e4d8688381cf5b45e6b16527b0176a169f0d55f
Arun VG. <arun.vg@ubtiinc.com>
2025-02-24 14:56:31 +0530

    fix: fixed program comments and gitignore file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+
+//declaring and initializing first two terms of the fibonacci series
 int a=0;
 int b=1;
+
+//Prompting the user for input
 Console.WriteLine("Enter the number of terms in Fibonacci Series:- ");
+
+//Storing the above input in the variable 'num' with string datatype 
 string num=Console.ReadLine()!;
+
+//Converting to string to int datatype
 int n=int.Parse(num);
+
+//if the input is valid condition
 if(n>0)
 {
     Console.WriteLine("The Fibonacci Series is: ");
@@ -17,7 +26,10 @@ if(n>0)
         a=b;
         b=c;
     }
+//fetches the fibonacci series if input is valid
 }
+
+//handles invalid input
 else
 {
     Console.WriteLine("Invalid Input");
