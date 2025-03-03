ccd2164dd5fcf58294482ccb8af8c0569515b027
Arun VG. <arun.vg@ubtiinc.com>
2025-03-03 12:55:39 +0530

    feat: Added input validation

--- Program.cs
+++ Program.cs
@@ -1,50 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-//declaring and initializing first two terms of the fibonacci series
-int a=0;
-int b=1;
-
-//Prompting the user for input
-Console.WriteLine("Enter the number of terms in Fibonacci Series:- ");
-
-//Storing the above input in the variable 'num' with string datatype 
-string num=Console.ReadLine()!;
-
-//Converting to string to int datatype
-int n=int.Parse(num);
-
-
-//try block
-try
+class Program
 {
-    if(n>0)
+    static void Main(string[] args)
     {
-        Console.WriteLine("The Fibonacci Series is: ");
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-        for(int i=2;i<n;i++)
+        while(true)
         {
-            int c=a+b;
-            Console.WriteLine(c);
-            a=b;
-            b=c;
+            //try block
+            try
+            {   
+                //declaring and initializing first two terms of the fibonacci series
+                int a=0;
+                int b=1;
+
+                //Prompting the user for input
+                Console.WriteLine("Enter the number of terms in Fibonacci Series:- ");
+
+                //Storing the above input in the variable 'num' with string datatype 
+                string num=Console.ReadLine()!;
+
+                if(string.IsNullOrEmpty(num)) //input validation block
+                {
+                    throw new Exception("Number of terms cannot be null! Please provide a valid input!");
+                }
+
+                //Converting to string to int datatype
+                int n=int.Parse(num);
+
+                if(n>0)
+                {
+                    Console.WriteLine("The Fibonacci Series is: ");
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    for(int i=2;i<n;i++)
+                    {
+                        int c=a+b;
+                        Console.WriteLine(c);
+                        a=b;
+                        b=c;
+                    }
+                //fetches the fibonacci series if input is valid
+                }
+                else
+                {
+                    throw new Exception("Invalid Input"); //throw statement -> throws an exception to catch block in case of errors
+                }
+            }
+
+            //catch block for exception handling
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                continue; //to continue with next iteration after handling errors
+            }
+
+            //finally block which gets executed irrespective of whether an exception is thrown or not
+            finally
+            {
+                Console.WriteLine("The execution has been completed.");
+            }
+            break;
         }
-    //fetches the fibonacci series if input is valid
     }
-    else
-    {
-        throw new Exception("Invalid Input"); //throw statement -> throws an exception to catch block in case of errors
-    }
-}
-
-//catch block for exception handling
-catch(Exception e)
-{
-    Console.WriteLine(e.Message);
-}
-
-//finally block which gets executed irrespective of whether an exception is thrown or not
-finally
-{
-    Console.WriteLine("The execution has been completed.");
 }
